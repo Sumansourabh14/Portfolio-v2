f9c20f50524178da40947c96f179501e2f471a45
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-11-30 23:56:34 +0530

    added contact styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesUp } from "@fortawesome/free-solid-svg-icons";
+import SocialMediaIcons from "./small-components/SocialMediaIcons";
 
 export default function Contact() {
   return (
@@ -7,31 +8,51 @@ export default function Contact() {
       <div className="container mx-auto px-12 py-40 gap-10">
         <div className="flex flex-col gap-10 text-center max-w-3xl mx-auto">
           <h2 className="text-3xl font-bold lg:text-5xl">Contact Me</h2>
-          <p className="text-slate-300">
-            If you like my work or want to hire me then please feel free to
-            shoot me an email on{" "}
-            <span className="bg-cyan-900 px-1 py-1">
-              sumsourabh14@gmail.com
-            </span>{" "}
-            or simply press the button below.
+          <p className="text-zinc-700">
+            If you like my work or want to hire me, please feel free to shoot me
+            an email on{" "}
+            <span className="underline">sumsourabh14@gmail.com</span> by
+            pressing the button below.
           </p>
           <div>
             <a
               href="mailto:sumsourabh14@gmail.com"
-              className="bg-gray-200 text-black font-bold px-10 py-4 rounded hover:bg-cyan-800 hover:text-white duration-200"
+              className="bg-black text-white font-bold px-10 py-4 hover:bg-zinc-800 duration-200"
             >
-              Contact me!
+              Send me an email!
             </a>
           </div>
+          <div className="flex flex-col gap-4">
+            <p>
+              or contact me through{" "}
+              <a
+                href="https://www.linkedin.com/in/sumansourabh14"
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>{" "}
+              or{" "}
+              <a
+                href="https://twitter.com/sumansourabhdev"
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Twitter
+              </a>
+            </p>
+          </div>
         </div>
-        <div className="text-right">
+        {/* <div className="text-right">
           <a href="#home" className="shadow-xl">
             <FontAwesomeIcon
               icon={faAnglesUp}
               className="animate-bounce text-2xl lg:text-4xl bg-gray-900 rounded-full p-2 hover:bg-sky-700 duration-200"
             />
           </a>
-        </div>
+        </div> */}
       </div>
     </section>
   );
